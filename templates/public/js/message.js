2c8f0cf05b23374689797510fa6d19a6f1cea50c
Tron <moutain1@126.com>
2014-03-20 11:13:26 +0800

    None

--- templates/public/js/message.js
+++ templates/public/js/message.js
@@ -1,13 +1,13 @@
 function connected(id){
 	createXMLRequest();                       
 	var message=document.getElementById("message"+id).value;
-	var messageurl="http://192.168.1.120:8866/users/message/?message="+message+"&to="+id;
+	var messageurl="http://192.168.1.113:8866/users/message/?message="+message+"&to="+id;
 	xmlHttp.open('get',messageurl);
-	xmlHttp.onreadystatechange=response;
+	xmlHttp.onreadystatechange=response(id);
 	xmlHttp.send(); 
 }
 
-function response(){
+function response(id){
 	if(xmlHttp.readyState==4 && xmlHttp.status==200){
 		result=document.getElementById('messageresult'+id);
 		result.innerHTML=xmlHttp.responseText;
